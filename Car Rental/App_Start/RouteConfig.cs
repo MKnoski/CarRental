a669ef35f8c6563081ba2b8ad4dbbf73184834ed
Maks <maks.knoski@gmail.com>
2016-10-23 20:44:05 +0200

    Solution adaptation

--- Car Rental/App_Start/RouteConfig.cs
+++ Car Rental/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Car_Rental
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CarRental", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Car Rental/Controllers/CarRentalController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Car_Rental.Controllers
+{
+    public class CarRentalController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
