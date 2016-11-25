08405f77b2004ec807f3116e52b46c60baf809a9
Maks <maks.knoski@gmail.com>
2016-11-25 10:58:55 +0100

    changes in authentication

--- Car Rental/Controllers/CarRentalController.cs
+++ Car Rental/Controllers/CarRentalController.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿using System.Net;
 using System.Web.Mvc;
-using CarRental.Data.Services.Interfaces;
-using Car_Rental.ViewModels;
-using CarRental.Data.Context;
-using CarRental.Data.Services;
 using Car_Rental.Models;
 using Car_Rental.Services.Interfaces;
 using Microsoft.AspNet.Identity;
 
 namespace Car_Rental.Controllers
 {
-    [Authorize]
     public class CarRentalController : Controller
     {
         private readonly ICarRentalsQuery carRentalsQuery;
@@ -27,14 +18,12 @@ namespace Car_Rental.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public ActionResult Index()
         {
             return View("Index");
         }
 
         [HttpGet]
-        [AllowAnonymous]
         public JsonResult GetRentals()
         {
             var viewModel = this.carRentalsQuery.GetCarRentals();
@@ -45,6 +34,11 @@ namespace Car_Rental.Controllers
         [HttpPost]
         public ActionResult AddRental(RentalModel model)
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            }
+
             var userId = this.User.Identity.GetUserId();
 
             model.ApplicationUserId = userId;
